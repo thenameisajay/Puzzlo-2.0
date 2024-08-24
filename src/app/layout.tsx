0cf40f91dd6a0dcce0e7ce4625aefcb53a2d3698
thenameisajay <m.ajaypradeep@icloud.com>
2024-08-24 16:54:57 +0100

    viewport added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           type="image/<generated>"
           sizes="<generated>"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={`${MonaSans.className} bg-sky-500`}>
         <Providers>
