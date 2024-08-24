e4e4a62ae43d0de4d17db8d7fdf4d6d1c68c905e
thenameisajay <m.ajaypradeep@icloud.com>
2024-08-24 19:32:37 +0100

    data types for navbar is ready

---
+++ src/data/navbar/data.ts
@@ -0,0 +1,20 @@
+import type { MainNavItem } from '@/types/interfaces/navbar/types';
+
+export const navConfig: MainNavItem[] = [
+  {
+    title: 'Home',
+    href: '/',
+  },
+  {
+    title: 'How to Play',
+    href: '/how-to-play',
+  },
+  {
+    title: 'Play',
+    href: '/play',
+  },
+  {
+    title: 'Scoreboard',
+    href: '/scoreboard',
+  },
+];
