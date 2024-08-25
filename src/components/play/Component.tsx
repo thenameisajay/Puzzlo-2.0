b161500d36c99534d367dd10bcc9bcfa0e695625
thenameisajay <m.ajaypradeep@icloud.com>
2024-08-25 11:14:49 +0100

    puzzlo ring custom

--- src/components/play/Component.tsx
+++ src/components/play/Component.tsx
@@ -2,20 +2,32 @@
 
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 import { ArrowCircleDown, ArrowCircleUp } from '@phosphor-icons/react';
 import confetti from 'canvas-confetti';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import { Button } from '@/components/ui/button';
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import {
   InputOTP,
   InputOTPGroup,
   InputOTPSlot,
 } from '@/components/ui/input-otp';
+import { Label } from '@/components/ui/label';
 
 dayjs.extend(utc);
 
 export default function PlayComponent({ password }: { password: number }) {
+  const router = useRouter();
   const [value, setValue] = useState('');
   const [isPasswordLower, setIsPasswordLower] = useState<boolean | null>(null);
   const [correctPassword, setCorrectPassword] = useState<boolean | undefined>(
@@ -25,9 +37,12 @@ export default function PlayComponent({ password }: { password: number }) {
   const [secondsElapsed, setSecondsElapsed] = useState(0);
   const [tries, setTries] = useState(1);
   const [score, setScore] = useState(0);
+  const [showDialog, setShowDialog] = useState(false);
+  const [username, setUsername] = useState('');
 
-  const puzzloCustomStyle =
-    '2xl:text-6xl h-16 w-16 text-base text-white md:text-xl lg:h-28 lg:w-28 lg:text-4xl xl:h-32 xl:w-32 bg-white text-yellow-500 ';
+  const puzzloCustomStyle = `2xl:text-6xl h-16 w-16 text-base text-white md:text-xl lg:h-28 lg:w-28 lg:text-4xl xl:h-32 xl:w-32 bg-white text-yellow-500 
+    focus:ring-2 col-span-3 bg-white text-yellow-500 ring-yellow-500 focus:ring-yellow-500 focus-visible:ring-yellow-500 active:ring-2 active:ring-yellow-500
+    `;
 
   const arrowCustomStyle =
     'animate-pulse text-7xl text-yellow-500 lg:text-8xl ';
@@ -62,6 +77,20 @@ export default function PlayComponent({ password }: { password: number }) {
     };
 
     frame();
+  };
+
+  const handleSave = () => {
+    // Here you would typically save the score to a database
+    console.log(`Saved score for ${username}: ${tries} tries`);
+    setShowDialog(false);
+  };
+
+  const handleExit = () => {
+    setShowDialog(false);
+    // Reset the game
+    router.push('/');
+    setTries(0);
+    setSecondsElapsed(0);
   };
 
   const calculateScore = (tries: number, secondsElapsed: number) => {
@@ -106,6 +135,7 @@ export default function PlayComponent({ password }: { password: number }) {
       setCorrectPassword(true);
       const localScore = calculateScore(tries, secondsElapsed);
       setScore(localScore);
+      setShowDialog(true);
     } else if (value < password.toString()) {
       setTries(tries + 1);
       setIsPasswordLower(true);
@@ -202,6 +232,55 @@ export default function PlayComponent({ password }: { password: number }) {
           Enter
         </Button>
       </div>
+      <Dialog open={showDialog} onOpenChange={setShowDialog}>
+        <DialogContent
+          className="border-2
+        border-yellow-500
+        bg-sky-500 sm:max-w-[425px]
+        "
+        >
+          <DialogHeader>
+            <DialogTitle className="text-2xl text-yellow-500 lg:text-3xl">
+              Congratulations!
+            </DialogTitle>
+            <DialogDescription className="text-base text-white">
+              You guessed the correct PIN in {tries} tries and your score is{' '}
+              {score}!
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right text-white">
+                Username
+              </Label>
+              <Input
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="col-span-3 bg-white
+                 text-yellow-500 ring-2 ring-yellow-500 focus:ring-2 focus:ring-yellow-500
+                 focus-visible:ring-yellow-500 active:ring-2  active:ring-yellow-500
+                "
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button
+              onClick={handleSave}
+              className="bg-yellow-500 text-lg text-white hover:bg-yellow-600"
+            >
+              Save
+            </Button>
+            <Button
+              onClick={handleExit}
+              variant="outline"
+              className="bg-white text-yellow-500 hover:bg-sky-100"
+            >
+              Exit
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
